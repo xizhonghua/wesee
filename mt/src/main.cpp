e2519699ee2fb385caa7c8c78d336fb13f173bc8
xizhonghua <xizhonghua@gmail.com>
2013-08-17 20:40:35 -0400

    init checkin for matting problem

---
+++ mt/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 17, 2013
+ *      Author: zhonghua
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+
+	cout<<"Hello World!"<<endl;
+
+	return 0;
+}

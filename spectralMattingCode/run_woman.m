810d716c0d20590fa74c36f2473e54f69a4d651e
xizhonghua <xizhonghua@gmail.com>
2013-08-23 23:27:30 -0400

    add spectralMattingCode

---
+++ spectralMattingCode/run_woman.m
@@ -0,0 +1,20 @@
+
+load my_images
+load scribbles
+%%
+% if this variable is set to 0, only the final alpha matting is saved.
+% Otherwise, partial results (such as the matting components) are saved.
+save_partial_results = 0;
+
+% try to group the matting components for forground/background matting
+do_grouping = 1;
+
+% for all examples here we use 50 eigen vectors...
+eigs_num=50;
+%%
+
+if (1)
+    nclust =10;
+    [alpha_comps,alpha] = SpectralMatting(my_images.woman, [], 'woman_', eigs_num, nclust, ...
+        [], save_partial_results);
+end
